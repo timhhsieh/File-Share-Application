58a0b3bc91a15e563ed0fb152c86ddf0cfd57aac
timhhsieh <timhsieh.erc@gmail.com>
2023-12-04 23:07:13 -0800

    now we can download
    added the ability to download the file from server side to client side

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 import os
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+available_files = {
+    'file1.txt': ['keyword1', 'keyword2'],
+    'file2.pdf': ['keyword3', 'keyword4'],
+    # Add more files and keywords as needed
+}
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -17,11 +23,27 @@ def upload_file():
 
     if file:
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        
+        # Update available files with keywords when a new file is uploaded
+        available_files[file.filename] = ['keyword_example']
+        
         return 'File uploaded successfully'
 
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
+
+@app.route('/available_files', methods=['GET'])
+def get_available_files():
+    return jsonify(list(available_files.keys()))
+
+@app.route('/keywords', methods=['GET'])
+def get_keywords():
+    keywords = set()
+    for file_keywords in available_files.values():
+        keywords.update(file_keywords)
+    return jsonify(list(keywords))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend.py
+++ frontend.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, ttk, messagebox
 import requests
 from tkinter import messagebox
 
@@ -12,6 +12,7 @@ def upload():
             response = requests.post('http://localhost:5000/upload', files=files)
             if response.status_code == 200:
                 messagebox.showinfo("Success", "File uploaded successfully")
+                show_available_files()  # Refresh available files after upload
             else:
                 messagebox.showerror("Error", "Failed to upload file")
 
@@ -24,6 +25,45 @@ def show_instructions():
     )
     messagebox.showinfo("Instructions", instructions)
 
+def fetch_available_files():
+    response = requests.get('http://localhost:5000/available_files')
+    if response.status_code == 200:
+        return response.json()
+    return []
+
+def fetch_keywords():
+    response = requests.get('http://localhost:5000/keywords')
+    if response.status_code == 200:
+        keywords = response.json()
+        # Save fetched keywords to a text file
+        with open('keywords.txt', 'w') as file:
+            file.write('\n'.join(keywords))
+        return keywords
+    return []
+
+def download_selected_file():
+    selected_file = download_dropdown.get()
+    if selected_file:
+        response = requests.get(f'http://localhost:5000/download/{selected_file}', stream=True)
+        if response.status_code == 200:
+            download_path = filedialog.asksaveasfilename(initialfile=selected_file, defaultextension=".txt")
+            if download_path:
+                with open(download_path, 'wb') as file:
+                    for chunk in response.iter_content(chunk_size=1024):
+                        if chunk:
+                            file.write(chunk)
+                messagebox.showinfo("Download Complete", f"File '{selected_file}' downloaded successfully.")
+            else:
+                messagebox.showwarning("Download Canceled", "Download canceled by the user.")
+        else:
+            messagebox.showerror("Download Failed", f"Failed to download '{selected_file}'.")
+
+
+def show_available_files():
+    files = fetch_available_files()
+    if files:
+        download_dropdown['values'] = files
+
 root = tk.Tk()
 root.title("File Sharing Service")
 
@@ -35,5 +75,11 @@ upload_button.pack()
 
 instructions_button = tk.Button(root, text="How to Use", command=show_instructions, width=20)
 instructions_button.pack(pady=10)
+
+download_dropdown = ttk.Combobox(root, width=30)
+download_dropdown.pack()
+
+download_button = tk.Button(root, text="Download", command=download_selected_file, width=20)
+download_button.pack(pady=10)
 
 root.mainloop()
